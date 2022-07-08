fcfcb45e7fd26d252edb8463f690b3aeb8399d55
Konstantin Schulz <Konstantin.Schulz@dfki.de>
2022-07-08 15:40:00 +0200

    fix mount order

--- train.sh
+++ train.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 NETSCRATCH="/netscratch/schulz"
-DATAROOT="$NETSCRATCH/question_answering_german"
-DATADIR="$DATAROOT/exp"
+DATADIR="$NETSCRATCH/question_answering_german/exp"
 HF_DATASETS_CACHE="$NETSCRATCH/hf_datasets_cache"
 TRANSFORMERS_CACHE="$NETSCRATCH/transformers_cache"
 ROOTDIR="`dirname \"$0\"`"
@@ -11,7 +10,6 @@ ENROOT_DIR="/netscratch/enroot"
 USER_IMAGES_DIR="$NETSCRATCH/docker_images"
 srun --container-image=$USER_IMAGES_DIR/$BASE_IMAGE \  # $ENROOT_DIR
   --container-workdir="`pwd`" \
-  --container-mounts=$NETSCRATCH:$NETSCRATCH,/ds:/ds:ro,"`pwd`":"`pwd`" \
+  --container-mounts="`pwd`":"`pwd`",/ds:/ds:ro,$NETSCRATCH:$NETSCRATCH \
+  --container-save=$USER_IMAGES_DIR/$BASE_IMAGE \
   ./env.sh && pip3 --no-cache-dir install -r requirements.txt && python germanQuAD_train.py
-
-#  --container-save=$USER_IMAGES_DIR/$BASE_IMAGE \
